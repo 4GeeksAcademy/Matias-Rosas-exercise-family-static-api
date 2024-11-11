efb068911fb07aa24f9e90e1046e78963cf32962
MatiRosas31 <165863634+MatiRosas31@users.noreply.github.com>
2024-11-11 20:19:31 +0000

    [Proyecto completado] -Se crearon: 1) El endpoint para traer a todos los miembros de la familia. Para la cual se utilizo el metodo de la clase "get_all_members()" 2) El endpoint para agregar/post un nuevo miembro. Y se modifico el metodo "add_member". Tambien se utilizo el metodo ya creado para generar una ID 3)El endpoint para eliminar miembros de la familia segun el ID. Para ello se modifico el metodo "delete_member(id)". 4)El endpoint para traer a un miembro en especifico de la familia segun su id 5) Para agregar a "Tommy" usando el metodo GET. Que utiliza el metodo "add_member" ya creado anteriormente
    -Todas las endpoints fueron probadas

--- src/app.py
+++ src/app.py
@@ -35,8 +35,38 @@ def handle_hello():
         "family": members
     }
 
+    return jsonify(response_body['family']), 200
 
-    return jsonify(response_body), 200
+@app.route('/member', methods=['POST'])
+def add_member():
+    members = jackson_family.get_all_members()
+    request_body  = request.json
+    request_body['id'] = jackson_family._generateId()
+    jackson_family.add_member(request_body)
+    return jsonify(members), 200
+
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_member(id):
+    jackson_family.delete_member(id)
+    message = {"done": True}
+    return jsonify(message), 200
+
+@app.route('/member/<int:id>')
+def get_member(id):
+    selectedmember = jackson_family.get_member(id)
+    return jsonify(selectedmember)
+
+@app.route('/member/3443', methods=['GET'])
+def add_tommy():
+        newtommy = {
+            'id': 3443,
+            'first_name': 'Tommy',
+            'last_name': jackson_family.last_name,
+            'age': 12,
+            'lucky_numbers': [15,35,5]
+        }
+        jackson_family.add_member(newtommy)
+        return jsonify({'Nuevo Miembro: ': newtommy, 'La familia ahora es: ': jackson_family.get_all_members(), 'done': True})
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
